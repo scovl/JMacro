c8feb8b768b3a5b0d3ba8edfa2aab2d904c710cf
Vitor Lobo Ramos <lobocode@gmail.com>
2023-02-04 21:12:03 -0300

    add min macro with mouse and exit key

--- src/main/java/com/br/jmacro/Jmacro.java
+++ src/main/java/com/br/jmacro/Jmacro.java
@@ -9,11 +9,39 @@ import com.github.kwhat.jnativehook.NativeHookException;
 import com.github.kwhat.jnativehook.keyboard.NativeKeyEvent;
 import com.github.kwhat.jnativehook.keyboard.NativeKeyListener;
 
+// mouse
+import com.github.kwhat.jnativehook.mouse.NativeMouseEvent;
+
+
 public class Jmacro implements NativeKeyListener {
     public void nativeKeyPressed(NativeKeyEvent e) {
-        if (e.getKeyCode() == NativeKeyEvent.VC_8) {
+        if (e.getKeyCode() == NativeKeyEvent.VC_EQUALS) {
             GlobalScreen.postNativeEvent(new NativeKeyEvent(NativeKeyEvent.NATIVE_KEY_PRESSED,
-                    (int) System.currentTimeMillis(), 0, NativeKeyEvent.VC_9, '9'));
+                    (int) System.currentTimeMillis(), 0, NativeKeyEvent.VC_SPACE, NativeKeyEvent.CHAR_UNDEFINED));
+
+            // time sleep 5 miliseconds
+            try {
+                Thread.sleep(5);
+            } catch (InterruptedException e1) {
+                e1.printStackTrace();
+            }
+
+            // VC_8
+            GlobalScreen.postNativeEvent(new NativeKeyEvent(NativeKeyEvent.NATIVE_KEY_PRESSED,
+                    (int) System.currentTimeMillis(), 0, NativeKeyEvent.VC_8, NativeKeyEvent.CHAR_UNDEFINED));
+
+            // mouse moved = (946, 500)
+            GlobalScreen.postNativeEvent(new NativeMouseEvent(NativeMouseEvent.NATIVE_MOUSE_MOVED,
+                    (int) System.currentTimeMillis(), 946, 500, 0, NativeMouseEvent.BUTTON1));
+
+            // VC_v
+            GlobalScreen.postNativeEvent(new NativeKeyEvent(NativeKeyEvent.NATIVE_KEY_PRESSED,
+                    (int) System.currentTimeMillis(), 0, NativeKeyEvent.VC_V, NativeKeyEvent.CHAR_UNDEFINED));
+        }
+
+        // exit macro
+        if (e.getKeyCode() == NativeKeyEvent.VC_DELETE) {
+            System.exit(0);
         }
     }
 
