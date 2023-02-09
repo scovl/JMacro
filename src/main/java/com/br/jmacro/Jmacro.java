c89f92e560be2c5108af96296c91d1a437728ce0
Vitor Lobo Ramos <lobocode@gmail.com>
2023-02-09 07:25:11 -0300

    Improving code with intStream, @Override, bug s9045

--- src/main/java/com/br/jmacro/Jmacro.java
+++ src/main/java/com/br/jmacro/Jmacro.java
@@ -10,6 +10,7 @@ import com.github.kwhat.jnativehook.keyboard.NativeKeyListener;
 import java.io.FileInputStream;
 import java.io.IOException;
 
+import java.io.InputStream;
 import java.util.Properties;
 import java.util.logging.Level;
 import java.util.logging.LogManager;
@@ -19,15 +20,17 @@ import java.util.stream.IntStream;
 
 public class Jmacro implements NativeKeyListener {
 
-    private Properties prop = new Properties();
+    private final Properties prop = new Properties();
 
     public Jmacro() throws ConfigFileException {
-        try {
-            prop.load(new FileInputStream("configMacro.properties"));
+        try (InputStream inputStream = new FileInputStream("configMacro.properties")) {
+            prop.load(inputStream);
         } catch (IOException e) {
             throw new ConfigFileException("Error reading the config file.", e);
         }
     }
+
+    @Override
     public void nativeKeyPressed(NativeKeyEvent e) {
         int keyCode = e.getKeyCode();
 
@@ -35,24 +38,24 @@ public class Jmacro implements NativeKeyListener {
         // numberOfMacros variable is used to define the number of macros that will be used.
         int numberOfMacros = Integer.parseInt(prop.getProperty("numberOfMacros"));
 
+        final String macro = "macro";
+
         // if numberOfMacros is greater than 1, then macro1_StartKey is "macro" + i + "_StartKey"
-        for (int i = 1; i <= numberOfMacros; i++) {
-            if (keyCode == Integer.parseInt(prop.getProperty("macro" + i + "_StartKey"))) {
-                int finalI = i;
-                IntStream.rangeClosed(1, Integer.parseInt(prop.getProperty("macro" + i + "_numberOfKeys")))
-                        .mapToObj(j -> {
-                            String keyValue = prop.getProperty("macro" + finalI + "_key" + j );
-                            if (keyValue == null || keyValue.isEmpty()) {
-                                throw new IllegalArgumentException("The " + "macro" + finalI + "_key" + j + " is not defined in the config file." );
-                            }
-                            return Integer.parseInt(keyValue);
-                        })
-                        .forEach(numCode -> {
-                            GlobalScreen.postNativeEvent(new NativeKeyEvent(NativeKeyEvent.NATIVE_KEY_PRESSED,
-                                    (int) System.currentTimeMillis(), 0, numCode, NativeKeyEvent.CHAR_UNDEFINED));
-                        });
-            }
-        }
+        IntStream.rangeClosed(1, numberOfMacros)
+                .forEach(i -> {
+                    if (keyCode == Integer.parseInt(prop.getProperty(macro + i + "_StartKey"))) {
+                        IntStream.rangeClosed(1, Integer.parseInt(prop.getProperty(macro + i + "_numberOfKeys")))
+                                .mapToObj(j -> {
+                                    String keyValue = prop.getProperty(macro + i + "_key" + j);
+                                    if (keyValue == null || keyValue.isEmpty()) {
+                                        throw new IllegalArgumentException("The " + macro + i + "_key" + j + " is not defined in the config file." );
+                                    }
+                                    return Integer.parseInt(keyValue);
+                                })
+                                .forEach(numCode -> GlobalScreen.postNativeEvent(new NativeKeyEvent(NativeKeyEvent.NATIVE_KEY_PRESSED,
+                                        (int) System.currentTimeMillis(), 0, numCode, NativeKeyEvent.CHAR_UNDEFINED)));
+                    }
+                });
 
         // exit macro
         if (e.getKeyCode() == Integer.parseInt(prop.getProperty("macroEndKey"))) {
@@ -60,11 +63,14 @@ public class Jmacro implements NativeKeyListener {
             System.exit(0);
         }
     }
-
+    @Override
     public void nativeKeyReleased(NativeKeyEvent e) {
+        // No code needed here for now
     }
 
+    @Override
     public void nativeKeyTyped(NativeKeyEvent e) {
+        // No code needed here for now
     }
     public static void main(String[] args) throws ConfigFileException {
         System.out.println("Jmacro is running...");
